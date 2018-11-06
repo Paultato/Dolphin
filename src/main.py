accbab9cea7026d81a5c71ba26423c55828ccdbe
Antoine LEGRAND <legrand.antoine96@gmail.com>
2018-11-06 19:01:37 +0100

    Ajout main

---
+++ src/main.py
@@ -0,0 +1,17 @@
+
+
+def main():
+	##PopulateDB
+		##get assets
+		##get ratios
+		##load ratios
+		##convert to EUR
+	##Algo
+		##create portfolio
+		##compute sharpe
+
+
+
+
+if __name__ == "__main__":
+	main()
