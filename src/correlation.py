26c11faeac163252dc9ba70f45c90be95d45864e
Paul Banus <paul.banus@epita.fr>
2018-11-29 15:32:00 +0100

    Correlation entre les asset sous CSV

--- src/correlation.py
+++ src/correlation.py
@@ -9,23 +9,39 @@ class CorrelationManager:
     db = dbManager()
     api = RestManager()
     assets = []
+    data = {}
 
     def __init__(self):
         db_assets = self.db.getAssets()
         for ass in db_assets:
             self.assets.append(ass.rest_id)
-        print(self.assets)
     
     def body(self, id):
         data = {"ratio" : [19], "asset" : self.assets, "benchmark" : id}
-        print(data)
         return data
     
     def get_corr(self, id):
         response = self.api.post("ratio/invoke", self.body(id))
         res = json.loads(response)
-        print(res)
-        return res
+        result = []
+        for ass in self.assets:
+            result.append(res[str(ass)][str(19)]['value'])
+        return result
+
+    def build_data(self):
+        for ass in self.assets:
+            self.data[ass] = self.get_corr(ass)
+    
+    def build_csv(self):
+        self.build_data()
+        doc = {'id': self.assets}
+        for ass in self.assets:
+            doc[ass] = self.data[ass]
+        df = pandas.DataFrame(doc)
+        print(df)
+        df.to_csv('corr.csv', sep=',', index=False, index_label=False)
+
+    
 
 cm = CorrelationManager()
-cm.body(597)
+#cm.build_csv()
