f69695c72b56e14350c8ce9727cf2032faf432dd
I. Marin <issam.marin@gmail.com>
2018-12-02 15:28:46 +0100

    Limite d'enfant réparée et changée par une limite de portfolios differents

--- src/SharpTree.py
+++ src/SharpTree.py
@@ -5,7 +5,7 @@ import time
 import queue
 import multiprocessing
 
-from portfolio import *
+# from portfolio import *
 from dbManager import dbManager
 from sqlalchemy import desc
 from decimal import Decimal
@@ -74,8 +74,8 @@ class nodeAsset:
     maxSharpe = Decimal(0.0)
     copySelf = self.deepCopy()
     path = createPath(self, None)
-    portfolio = Portfolio()
-    addPortfolio(portfolio, path)
+    # portfolio = Portfolio()
+    # addPortfolio(portfolio, path)
     tmp = list()
     for a in assetList:
       if not (a == self):
@@ -83,7 +83,7 @@ class nodeAsset:
     for asset in tmp:
       maxSharpe = max(maxSharpe, asset.sharpe)
       # Elagage sharpe
-      if (Decimal(asset.sharpe) > Decimal(maxSharpe) / Decimal(1.2) and (len(asset.children) < 3)):
+      if (Decimal(asset.sharpe) > Decimal(maxSharpe) / Decimal(1.2)):
         asset.parent = copySelf
         # Elagage unicité
         if (not pathList.count(createPath(asset, None))):
@@ -101,7 +101,7 @@ class nodeAsset:
         if (path.count('-') + 1 == height) and (not pathList.count(path)):
           pathList.append(path)
     for children in copySelf.childrens:
-      if (path.count('-') + 1 < height):
+      if (path.count('-') + 1 < height) and (len(pathList) < 30):
         children.sharpeTree(tmp, path, pathList, height)
 
   def sharpeTree2(self, assetList, path, pathList, height):
@@ -144,27 +144,27 @@ if __name__ == "__main__":
 
   assetList = list()
   db = dbManager()
-  query = db.getAssets().order_by(desc('sharpe')).limit(5).all()
+  query = db.getAssets().order_by(desc('sharpe')).limit(10).all()
   for asset in query:
     tmp = nodeAsset(asset.rest_id, asset.close_value + (asset.close_value_decimal / 1000000000000) - 1,
                     asset.asset_type, asset.sharpe)
     assetList.append(tmp)
   db.close()
 
-  pathList = queue.Queue()
+  pathList = list()
   threadPathList = queue.Queue()
 
   start = time.time()
-  assetList[0].sharpeTree2(assetList, "", pathList, 3)
+  assetList[0].sharpeTree(assetList, "", pathList, 4)
   end = time.time()
   print(end - start)
   
   start = time.time()
   jobs = []
-  for i in range(5):
-    p = multiprocessing.Process(target=worker, args=(assetList[i], assetList, threadPathList, 3,))
-    p.start()
-    jobs.append(p)
+  # for i in range(5):
+  #   p = multiprocessing.Process(target=worker, args=(assetList[i], assetList, threadPathList, 3,))
+  #   p.start()
+  #   jobs.append(p)
 
   end = time.time()
   print(end - start)
