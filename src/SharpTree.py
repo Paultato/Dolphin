23bd87f76ac82684a14af7dbcbccd5b658d93155
I. Marin <issam.marin@gmail.com>
2018-12-02 15:05:47 +0100

    sharptree multiprocess

--- src/SharpTree.py
+++ src/SharpTree.py
@@ -1,8 +1,30 @@
 import collections
 import json
-from dbManager import *
+import threading
+import time
+import queue
+import multiprocessing
+
+from dbManager import dbManager
 from sqlalchemy import desc
 from decimal import Decimal
+
+class CheckableQueue():  # or OrderedSetQueue
+  def __init__(self, source_queue):
+    self.source_queue = source_queue
+  def __contains__(self, item):
+    e = None
+    res = False
+    tmp = queue.Queue()
+    #with self.source_queue.mutex:
+    while len(self.source_queue.queue):
+      e = self.source_queue.get_nowait()
+      if e == item:
+        res = True
+      tmp.put(e)
+    while not tmp.empty():
+      self.source_queue.put(tmp.get())
+    return res
 
 
 def createPath(node, oldPath):
@@ -13,7 +35,6 @@ def createPath(node, oldPath):
   res = ""
   while node and not visited.count(node):
     visited.append(node)
-    # path.append("".join(sorted((json.loads(node.value)['name'].lower()))).strip())
     path.append(str((node.restId)).lower().strip())
     node = node.parent
   path.sort()
@@ -23,7 +44,6 @@ def createPath(node, oldPath):
   while path:
     res += '-' + path.pop()
   return res
-
 
 class nodeAsset:
   def __init__(self, restId, closeValue, type, sharpe):
@@ -44,10 +64,40 @@ class nodeAsset:
     tmp.childrens = list()
     return tmp
 
-  def prettyPrint(self):
-      createPath(self, None)
+  def sharpeTree(self, assetList, path, pathList, height):
+    maxSharpe = Decimal(0.0)
+    copySelf = self.deepCopy()
+    
+    path = createPath(self, None)
+    tmp = list()
+    for a in assetList:
+      if not (a == self):
+        tmp.append(a.deepCopy())
+    for asset in tmp:
+      maxSharpe = max(maxSharpe, asset.sharpe)
+      # Elagage sharpe
+      if (Decimal(asset.sharpe) > Decimal(maxSharpe) / Decimal(1.2)):
+        asset.parent = copySelf
+        # Elagage unicité
+        if (not pathList.count(createPath(asset, None))):
+          copySelf.childrens.append(asset)
+          if (path.count('-') + 1 == height) and (not pathList.count(path)):
+            pathList.append(path)
+        else:
+          asset.parent = None
+          #print("Elagage unicité")
+          continue
+      # else:
+        # print("Elagage sharpe")
+      if not copySelf.childrens:
+        # print("path : " + path)
+        if (path.count('-') + 1 == height) and (not pathList.count(path)):
+          pathList.append(path)
+    for children in copySelf.childrens:
+      if (path.count('-') + 1 < height):
+        children.sharpeTree(tmp, path, pathList, height)
 
-  def sharpeTree(self, assetList, path, pathList):
+  def sharpeTree2(self, assetList, path, pathList, height):
     maxSharpe = Decimal(0.0)
     copySelf = self.deepCopy()
     path = createPath(self, None)
@@ -58,41 +108,65 @@ class nodeAsset:
     for asset in tmp:
       maxSharpe = max(maxSharpe, asset.sharpe)
       # Elagage sharpe
-      if (Decimal(asset.sharpe) > Decimal(maxSharpe) / Decimal(1.2)):   
+      if (Decimal(asset.sharpe) > Decimal(maxSharpe) / Decimal(1.2)):
         asset.parent = copySelf
         # Elagage unicité
-        if (not pathList.count(createPath(asset, None))):
+        if (not createPath(asset, None) in CheckableQueue(pathList)):
           copySelf.childrens.append(asset)
-          if (path.count('-') + 1 == 20) and (not pathList.count(path)):
-            pathList.append(path)
+          if (path.count('-') + 1 == height) and (not path in CheckableQueue(pathList)):
+            pathList.put_nowait(path)
         else:
           asset.parent = None
-          print("Elagage unicité")
+          #print("Elagage unicité")
           continue
-      else:
-        print("Elagage sharpe")
+      # else:
+        # print("Elagage sharpe")
       if not copySelf.childrens:
-        print("path : " + path)
-        if (path.count('-') + 1 == 20) and (not pathList.count(path)):
+        # print("path : " + path)
+        if (path.count('-') + 1 == height) and (not pathList.count(path)):
           pathList.append(path)
     for children in copySelf.childrens:
-      if (path.count('-') + 1 < 20):
-        children.sharpeTree(tmp, path, pathList)
+      if (path.count('-') + 1 < height):
+        children.sharpeTree2(tmp, path, pathList, height)
 
+def worker(a, assetList, threadpathList, height):
+  a.sharpeTree2(assetList, "", threadPathList, height)
+  print(threadpathList)
 
 if __name__ == "__main__":
 
   assetList = list()
   db = dbManager()
-  query = db.getAssets().order_by(desc('sharpe')).limit(30).all()
+  query = db.getAssets().order_by(desc('sharpe')).limit(5).all()
   for asset in query:
     tmp = nodeAsset(asset.rest_id, asset.close_value + (asset.close_value_decimal / 1000000000000) - 1,
                     asset.asset_type, asset.sharpe)
     assetList.append(tmp)
   db.close()
 
-  pathList = list()
-  a = assetList[0]
-  a.sharpeTree(assetList, "", pathList)
-  for p in pathList:
-    print(p)
+  pathList = queue.Queue()
+  threadPathList = queue.Queue()
+
+  start = time.time()
+  assetList[0].sharpeTree2(assetList, "", pathList, 3)
+  end = time.time()
+  print(end - start)
+  
+  start = time.time()
+  jobs = []
+  for i in range(5):
+    p = multiprocessing.Process(target=worker, args=(assetList[i], assetList, threadPathList, 3,))
+    p.start()
+    jobs.append(p)
+
+  end = time.time()
+  print(end - start)
+
+i = 0
+threadPathList.put("toto")
+print(threadPathList.empty())
+while not threadPathList.empty():
+  a = threadPathList.get()
+  print(a)
+  print(threadPathList.empty())
+print("out")
